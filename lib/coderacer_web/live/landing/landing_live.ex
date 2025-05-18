5efaf213f72d1356ae9921e70de9951a0b56f1ed
Riza Fahmi <rizafahmi@gmail.com>
2025-05-18 15:22:20 +0700

    Finish save state

--- lib/coderacer_web/live/landing/landing_live.ex
+++ lib/coderacer_web/live/landing/landing_live.ex
@@ -3,8 +3,12 @@ defmodule CoderacerWeb.LandingLive do
 
   require Logger
 
+  @agent_name :score
+
   @impl true
   def mount(_params, _session, socket) do
+    initial_state = %{streak: 0, wrong: 0}
+
     code = "console.log(\"Hello, world!\")"
 
     socket =
@@ -12,42 +16,60 @@ defmodule CoderacerWeb.LandingLive do
       |> assign(:code, code)
       |> assign(:remaining_code, code)
       |> assign(:current_char, "")
+      |> assign(:score, initial_state)
 
     {:ok, socket}
   end
 
   @impl true
   def handle_event("user_type", %{"typing" => current_char}, socket) do
-    char_to_check =
-      current_char
-      |> String.reverse()
-      |> String.graphemes()
-      |> List.first()
+    char_to_check = get_char_to_check(current_char)
 
     Logger.info("User typed: #{char_to_check}")
     Logger.info("Remaining code: #{socket.assigns.remaining_code}")
     remaining_code = socket.assigns.remaining_code
 
-    [h | t] = String.graphemes(remaining_code)
+    socket = check_code(String.graphemes(remaining_code), char_to_check, socket)
 
+    {:noreply, socket}
+  end
+
+  def get_char_to_check(current_char) do
+    current_char
+    |> String.reverse()
+    |> String.graphemes()
+    |> List.first()
+  end
+
+  def check_code([], _char_to_check, socket) do
+    # No characters left to check
+    Logger.info("Finish")
+    socket
+  end
+
+  def check_code([h | t], char_to_check, socket) do
     case char_to_check == h do
       true ->
-        Logger.info("Correct character typed: #{h}")
-
-        remaining_code = t |> Enum.join()
+        # Correct character typed
+        new_remaining_code = Enum.join(t)
 
         socket =
           socket
-          |> assign(:remaining_code, remaining_code)
+          |> assign(:remaining_code, new_remaining_code)
+          |> assign(:score, %{
+            streak: socket.assigns.score.streak + 1,
+            wrong: socket.assigns.score.wrong
+          })
 
-        {:noreply, socket}
+        socket
 
-      _ ->
-        Logger.error("Incorrect character typed: #{current_char}")
+      false ->
+        # Incorrect character typed
+        socket =
+          socket
+          |> assign(:score, %{streak: 0, wrong: socket.assigns.score.wrong + 1})
 
-        {:noreply, socket}
+        socket
     end
-
-    # Current char
   end
 end
