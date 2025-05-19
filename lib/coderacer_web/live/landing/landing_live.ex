8e63124b09d4d17b56560c34e043503d7d0114d7
Riza Fahmi <rizafahmi@gmail.com>
2025-05-19 20:59:11 +0700

    Add code generation and update UI for typing game
    - Introduce "gen-code" event to generate code dynamically
    - Replace input field with a textarea for user typing
    - Add a button to start code generation
    - Update UI to display only remaining code

--- lib/coderacer_web/live/landing/landing_live.ex
+++ lib/coderacer_web/live/landing/landing_live.ex
@@ -7,17 +7,31 @@ defmodule CoderacerWeb.LandingLive do
   def mount(_params, _session, socket) do
     initial_state = %{streak: 0, wrong: 0}
 
-    code = "console.log(\"Hello, world!\")"
+    # code = Coderacer.AI.generate("JavaScript", 2, "easy")
+    # code =
+    #   "const arr = [];\nfor (let i = 0; i < 10; i++) {\n  arr.push(i * 2);\n}\n\nfunction greet(name) {\n  console.log('Hello, ' + name + '!');\n}\n\ngreet('World');\n\nconst obj = {\n  name: 'Example',\n  value: 123\n};\n\nconsole.log(obj.name);\nconsole.log(arr);\n"
 
     socket =
       socket
-      |> assign(:code, code)
-      |> assign(:remaining_code, code)
+      # |> assign(:code, code)
+      |> assign(:remaining_code, "")
       |> assign(:current_char, "")
       |> assign(:score, initial_state)
       |> assign(:elapsed_time, %{elapsed_time: 0, running: false})
 
     {:ok, socket}
+  end
+
+  @impl true
+  def handle_event("gen-code", _value, socket) do
+    # code = "const arr = [];"
+    code = Coderacer.AI.generate("Clojure", 15, "easy")
+
+    socket =
+      socket
+      |> assign(:remaining_code, code)
+
+    {:noreply, socket}
   end
 
   @impl true
