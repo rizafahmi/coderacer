adca9507e52bd2ee568d38f0f556704833087b9c
Riza Fahmi <rizafahmi@gmail.com>
2025-05-19 20:57:49 +0700

    Update AI.generate/2 test to include additional arguments

--- test/coderacer/ai_test.exs
+++ test/coderacer/ai_test.exs
@@ -2,9 +2,11 @@ defmodule Coderacer.AITest do
   use ExUnit.Case
 
   test "generate/2 returns valid code for supported language and difficulty" do
-    code = Coderacer.AI.generate("JavaScript", "easy")
+    code = Coderacer.AI.generate("JavaScript", 2, "easy")
 
     assert is_binary(code), "Expected generated code to be a binary string"
     assert String.length(code) > 0, "Expected generated code to be non-empty"
   end
+
+  test "to make sure generate/2 retunrs only code and not some markdown triple tick"
 end
